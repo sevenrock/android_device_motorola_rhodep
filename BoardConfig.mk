a8516e7387519efaf5945f292d263f4ac369e7dd
Marc Bourgoin <themard85@gmail.com>
2025-05-10 19:36:57 -0600

    rhodep: Add Motorola imgtuner to device framework matrix
    Change-Id: I811f62327c7a33b80845a66306471a0315c9927d
    
    Signed-off-by: Sevenrock <sevenrock@hotmail.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,8 @@ ODM_MANIFEST_B_FILES := $(DEVICE_PATH)/sku/manifest_b.xml
 ODM_MANIFEST_D_FILES := $(DEVICE_PATH)/sku/manifest_d.xml
 ODM_MANIFEST_DN_FILES := $(DEVICE_PATH)/sku/manifest_dn.xml
 ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/device_framework_matrix.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hab.product=rhodep
